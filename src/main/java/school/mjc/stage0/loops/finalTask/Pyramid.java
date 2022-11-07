f06bc0c5ce4e2aca59de9b7522a21b2879d72cac
alexdarkovich <alexdarkovich@mail.ru>
2022-11-07 19:49:34 +0300

    required code written

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,6 +2,22 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        int rowCount = 1;
+        for (int i = cathetusLength; i > 0; i--) {
+
+            for (int j = 1; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = rowCount; j >= 1; j--) {
+                System.out.print(j);
+            }
+            for (int j = 1; j <= rowCount-1; j++) {
+                System.out.print(j+1);
+            }
+
+            System.out.println();
+            rowCount++;
+        }
 
     }
 
